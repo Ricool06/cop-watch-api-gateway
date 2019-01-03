7d5212daf106c401d245dd3adc3f068b8f6a8fe5
Ricool06 <ricoolohsix@hotmail.com>
2019-01-03 01:59:16 +0000

    Amend env vars in config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   app: {
-    publishSocketPort: Number(process.env.REQUEST_SOCKET_PORT) || 9091,
-    subscribeSocketPort: Number(process.env.REQUEST_SOCKET_PORT) || 9090,
+    publishSocketPort: Number(process.env.PUBLISH_SOCKET_PORT) || 9091,
+    subscribeSocketPort: Number(process.env.SUBSCRIBE_SOCKET_PORT) || 9090,
     httpPort: Number(process.env.HTTP_PORT) || 8090,
     httpEndpoint: process.env.HTTP_ENDPOINT || '/graphql',
   },
